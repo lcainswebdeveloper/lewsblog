76bb5555cc48194bf05be25e1cd4e852f5bc89e5
Lewis Cains <lewis_cains@hotmail.com>
2018-08-03 09:45:24 +0100

    env file added

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import _ from 'lodash';
 import $ from 'jquery';
 window._ = _;
 window.$ = $;
+import ENV from './env';
 
 import axios from 'axios'; 
-axios.defaults.baseURL = apiBaseUrl;
+axios.defaults.baseURL = ENV.blogApiUrl();
 window.axios = axios;
-console.log(axios.defaults);
+
 import Flash from './tools/Flash';
 window.Flash = Flash;
 
@@ -18,6 +19,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 require('./vue-filters.js');
+
 /*Autoload our vue components*/
 require('./autoload.js');
 
